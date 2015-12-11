c175fde9fcc38c173d4ab12b7914964f96ba1eaa
rainbird <tara@chello.at>
2015-12-11 04:13:04 +0100

    some procedure implementations and their tests
    matrix, network and neural-network analysis, and tests

--- src/test/java/qube/qai/procedure/TestNeuralNetworkAnalysis.java
+++ src/test/java/qube/qai/procedure/TestNeuralNetworkAnalysis.java
@@ -1,6 +1,12 @@
 package qube.qai.procedure;
 
 import junit.framework.TestCase;
+import qube.qai.data.Arguments;
+import qube.qai.data.Metrics;
+import qube.qai.data.Selector;
+import qube.qai.data.selectors.DataSelector;
+import qube.qai.matrix.Matrix;
+import qube.qai.network.neural.NeuralNetwork;
 import qube.qai.procedure.analysis.NeuralNetworkAnalysis;
 
 /**
@@ -8,11 +14,63 @@ import qube.qai.procedure.analysis.NeuralNetworkAnalysis;
  */
 public class TestNeuralNetworkAnalysis extends TestCase {
 
-    public void testMatrixAnalysis() throws Exception {
-        NeuralNetworkAnalysis neuralNetworkAnalysis = (NeuralNetworkAnalysis) NeuralNetworkAnalysis.Factory.constructProcedure();
+    private boolean debug = true;
 
-        assertNotNull("Factory created no object at all", neuralNetworkAnalysis);
+    public void testNeuralNetworkAnalysis() throws Exception {
 
-        fail("test not yet implemented");
+        NeuralNetworkAnalysis statistics = new NeuralNetworkAnalysis();
+
+        Arguments arguments = statistics.getArguments();
+        assertNotNull("arguments may not be null", arguments);
+
+        Matrix matrix = Matrix.createMatrix(true, 100, 100);
+        NeuralNetwork network = new NeuralNetwork();
+        network.buildFromAdjacencyMatrix(matrix);
+
+        Selector<NeuralNetwork> selector = new DataSelector<NeuralNetwork>(network);
+        statistics.getArguments().setArgument(NeuralNetworkAnalysis.INPUT_NEURAL_NETWORK, selector);
+
+        checkResultsOf(statistics);
+
+        assertTrue("there has to be some results", !statistics.getArguments().getResultNames().isEmpty());
+        log("results:" + statistics.getArguments().getResultNames());
+        for (String name : statistics.getArguments().getResultNames()) {
+            Object result = statistics.getArguments().getResult(name);
+            if (result instanceof Metrics) {
+                log("found metrics: " + name);
+                log(((Metrics)result).toString());
+            } else {
+                log("result: " + result + " value: " + result);
+            }
+        }
+    }
+
+    /**
+     * run the given procedure and check that every
+     * result parameter mentioned in the procedure
+     * is actually to be found in the arguments
+     * @param statistics
+     */
+    private void checkResultsOf(ProcedureChain statistics) {
+
+        long start = System.currentTimeMillis();
+
+        statistics.run();
+        long duration = System.currentTimeMillis() - start;
+
+        log("Process: " + statistics.getName() + " took " + duration + "ms");
+
+        assertTrue("there has to be result names", !statistics.getArguments().getResultNames().isEmpty());
+        // assert also that the given results can actually be accessed
+        for (String resultName : statistics.getArguments().getResultNames()) {
+            assertTrue("result: '" + resultName + "' missing", statistics.getArguments().getResult(resultName) != null);
+        }
+    }
+
+    private void log(String message) {
+        if (debug) {
+            System.out.println(message);
+            //logger.info(message);
+        }
     }
 }
