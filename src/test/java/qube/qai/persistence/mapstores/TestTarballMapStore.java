ca33baf59f43caa0e886cab3cb02741972d989aa
rainbird <tara@chello.at>
2015-12-24 08:08:08 +0100

    getting branches right

--- src/test/java/qube/qai/persistence/mapstores/TestTarballMapStore.java
+++ src/test/java/qube/qai/persistence/mapstores/TestTarballMapStore.java
@@ -1,7 +1,20 @@
 package qube.qai.persistence.mapstores;
 
+import junit.framework.TestCase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by rainbird on 12/24/15.
  */
-public class TestTarballMapStore {
+public class TestTarballMapStore extends TestCase {
+
+    private static Logger logger = LoggerFactory.getLogger("TestTarballMapStore");
+
+    private static String TarballFile = "";
+
+    public void testTarballMapStore() throws Exception {
+
+        TarballMapStore mapStore = new TarballMapStore();
+    }
 }
