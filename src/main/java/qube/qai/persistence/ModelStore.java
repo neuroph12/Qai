412963208b399311cc119d21d1c77d656a52c400
zenpunk <zenpunk137@gmail.com>
2017-02-28 13:54:52 +0100

    copyright...

--- src/main/java/qube/qai/persistence/ModelStore.java
+++ src/main/java/qube/qai/persistence/ModelStore.java
@@ -1,14 +1,32 @@
+/*
+ * Copyright 2017 Qoan Software Association. All rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ *
+ */
+
 package qube.qai.persistence;
 
 import org.apache.jena.query.Dataset;
 import org.apache.jena.query.ReadWrite;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.tdb.TDBFactory;
+import qube.qai.services.DataServiceInterface;
+import qube.qai.services.implementation.SearchResult;
+
+import java.util.Collection;
 
 /**
  * Created by rainbird on 1/20/17.
  */
-public class ModelStore {
+public class ModelStore implements DataServiceInterface {
 
     private String directoryName;
 
@@ -30,5 +48,25 @@ public class ModelStore {
         dataset.begin(ReadWrite.WRITE);
         model = dataset.getDefaultModel();
         dataset.end();
+    }
+
+    @Override
+    public Collection<SearchResult> searchInputString(String searchString, String fieldName, int hitsPerPage) {
+        return null;
+    }
+
+    @Override
+    public WikiArticle retrieveDocumentContentFromZipFile(String fileName) {
+        return null;
+    }
+
+    @Override
+    public void save(Model model) {
+
+    }
+
+    @Override
+    public void remove(Model model) {
+
     }
 }
--- src/test/java/qube/qai/persistence/TestModelStore.java
+++ src/test/java/qube/qai/persistence/TestModelStore.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright 2017 Qoan Software Association. All rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ *
+ */
+
 package qube.qai.persistence;
 
 import junit.framework.TestCase;
@@ -11,6 +25,8 @@ public class TestModelStore extends TestCase {
 
         ModelStore modelStore = new ModelStore("./test/dummy.model.directory");
         modelStore.init();
+
+        fail("this test is not yet complete");
 
     }
 }
